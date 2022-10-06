c4ce1814692f5c51f83b687c8fb920feb47ffce7
Syamkumarramanatham <syamkumar.ramanadham@gmail.com>
2022-10-06 21:06:15 +0530

    UI changes

--- src/app/list-all-companies/list-all-companies.component.ts
+++ src/app/list-all-companies/list-all-companies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionCompany } from '../model/transaction-company.model';
 import { EStockMarketService } from '../service/e-stock-market.service';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-list-all-companies',
@@ -9,13 +10,12 @@ import { EStockMarketService } from '../service/e-stock-market.service';
 })
 export class ListAllCompaniesComponent implements OnInit {
   responseCompanies: any = [];
-
+  alert: boolean = false;
   constructor(private service: EStockMarketService) {}
 
   ngOnInit(): void {
    
     this.service.getAllCompaniesWithStocks().subscribe((data) => {
-  
       console.warn(data);
       this.responseCompanies = data;
     });
@@ -26,6 +26,12 @@ export class ListAllCompaniesComponent implements OnInit {
       const deletedCompany = this.responseCompanies.find((x: any) => { x.code === companyCode });
       this.responseCompanies.splice(this.responseCompanies.indexOf(deletedCompany), 1)
       console.warn('Company delete succesfully');
+      this.alert = true;
     })
   }
+  closeAlert() {
+    this.alert = false;
+    this.ngOnInit();
+  }
+
 }
